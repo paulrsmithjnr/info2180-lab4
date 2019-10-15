6cd59449ebbc13481fb6cd03370b298caef94eff
Paul Smith <paulrsmithjnr@gmail.com>
2019-10-14 23:32:15 -0500

    Added the X's and O's to the grid via javascript

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,8 +1,33 @@
 window.onload = function() {
      var board = document.getElementById("board");
      var children = board.children;
+     var flag = true;
+     var gameState = ["", "", "", "", "", "", "", "", ""]
 
      for (let i = 0; i < children.length; i++){
          children[i].classList.add("square");
+         children[i].onclick = function () {whenClicked(children[i], i)};
      }
+
+     function whenClicked(child, index) {
+         if(gameState[index] === "") {
+             
+             if(flag === true) {
+                child.innerHTML = "X";
+                child.classList.add("X")
+                gameState[index] = "X"
+                flag = false;
+             } else if(flag == false) {
+                child.innerHTML = "O";
+                child.classList.add("O")
+                gameState[index] = "O"
+                flag = true;   
+             } 
+
+         }
+         
+     }
+
+     
+
 };
